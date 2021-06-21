ad3283ccf457ab384402a765d05a248b2e2f2dc2
Sameer Shaikh <sameer.shaikh@ibm.com>
2021-06-21 13:40:34 +0530

    Review Comments

--- scripts/build-bin.sh
+++ scripts/build-bin.sh
@@ -1,11 +1,20 @@
-#!/bin/bash
-# ******************************************************************************
-# * Licensed Materials - Property of IBM
-# * IBM Cloud Container Service, 5737-D43
-# * (C) Copyright IBM Corp. 2018, 2019 All Rights Reserved.
-# * US Government Users Restricted Rights - Use, duplication or
-# * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-# ******************************************************************************
+/**
+ *
+ * Copyright 2021- IBM Inc. All rights reserved
+ * SPDX-License-Identifier: Apache2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 set -e
 set +x
