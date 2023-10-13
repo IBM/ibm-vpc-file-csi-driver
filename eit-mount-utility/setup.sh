42a7ca24b9611bab7175bd93e9b0378017e59bb9
Prankul <84079249+prankulmahajan@users.noreply.github.com>
2023-10-13 12:10:23 +0530

    Run mount-container via setup.sh -- revert

--- eit-mount-utility/setup.sh
+++ eit-mount-utility/setup.sh
@@ -12,9 +12,24 @@
 set -ex
 
 if [ "$IS_NODE_SERVER" = "true" ]; then
+    echo "Copying mount-helper-container files..."
+    cp  /home/ibm-csi-drivers/eitserver-linux /host/usr/local/bin/
+    cp /home/ibm-csi-drivers/mount-helper-container.service /host/etc/systemd/system/
     echo "Inside node-server. Enabling mount-helper-container service..."
     /home/ibm-csi-drivers/systemutil -action reload
     /home/ibm-csi-drivers/systemutil -target mount-helper-container.service -action start
+    # echo "Checking if the service is enabled..."
+    # if /home/ibm-csi-drivers/systemutil is-active --quiet mount-helper; then
+    #     echo "The 'mount-helper-container' service is running."
+    # else
+    #     echo "The 'mount-helper' service is not running. Retrying enabling..."
+    #     sleep 60
+    # fi
+
+    # Add logic to retry the service and confirm if ready. Try to handle it in a way to log the problem. 
+    # Try 2-3 retries for 5 min. 
+    # how it can be fixed.
+    # Node-server should show in events.
 fi
 
 # Run file-csi-driver binary with all the possible arguments passed from the deployment.
