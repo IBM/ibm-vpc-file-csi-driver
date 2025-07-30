e57b3903232b8e80178110fee73966490e3a4ba0
Renuka <renuka.singare@ibm.com>
2025-07-30 19:37:50 +0530

    Addressed comments
    Signed-off-by: Renuka <renuka.singare@ibm.com>

--- pkg/ibmcsidriver/controller_helper.go
+++ pkg/ibmcsidriver/controller_helper.go
@@ -335,14 +335,14 @@ func getVolumeParameters(logger *zap.Logger, req *csi.CreateVolumeRequest, confi
 	//TODO port the code from VPC BLOCK to find region if zone is given
 
 	// validate bandwidth for dp2 profile
-	if volume.VPCVolume.Profile != nil && volume.VPCVolume.Profile.Name == "dp2" && volume.VPCVolume.Bandwidth == 0 {
+	if volume.VPCVolume.Profile.Name == DP2Profile && volume.VPCVolume.Bandwidth > 0 {
 		err = fmt.Errorf("bandwidth must be specified for dp2 profile fileShare. provide a non-zero 'bandwidth' value in the storage class parameters")
 		logger.Error("getVolumeParameters", zap.NamedError("invalidParameter", err))
 		return volume, err
 	}
 
 	// validate zone for rfs profile
-	if volume.VPCVolume.Profile != nil && volume.VPCVolume.Profile.Name == "rfs" && len(strings.TrimSpace(volume.Az)) != 0 {
+	if volume.VPCVolume.Profile.Name == RFSProfile && len(strings.TrimSpace(volume.Az)) != 0 {
 		err = fmt.Errorf("zone must not be specified for rfs profile fileShare. remove 'zone' parameter from the storage class for rfs volumes")
 		logger.Error("getVolumeParameters", zap.NamedError("invalidParameter", err))
 		return volume, err
