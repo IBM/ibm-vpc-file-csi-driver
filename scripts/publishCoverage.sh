c8073eb7801b1fbb957f3000b2bf387d96ba880f
Mahantesh-R <mahantesh.r@ibm.com>
2025-09-02 18:20:03 +0530

    adds gtihub actions related items
    Signed-off-by: Mahantesh-R <mahantesh.r@ibm.com>

--- scripts/publishCoverage.sh
+++ scripts/publishCoverage.sh
@@ -17,8 +17,17 @@
 set -x
 echo "Publishing the coverage results"
 
+# GitHub Actions vars:
+# $GITHUB_WORKSPACE   -> root of repo
+# $GITHUB_REPOSITORY  -> owner/repo
+# $GITHUB_REF_NAME    -> branch or tag name
+# $GITHUB_SHA         -> current commit SHA
+# $GITHUB_RUN_NUMBER  -> CI run number
+# $GITHUB_EVENT_NAME  -> event type (push, pull_request, etc)
+# $GITHUB_EVENT_PATH  -> path to the full webhook event JSON
+
 mkdir -p "$GITHUB_WORKSPACE/gh-pages"
-cd "$GITHUB_WORKSPACE/gh-pages" || return
+cd "$GITHUB_WORKSPACE/gh-pages" || exit 1
 
 OLD_COVERAGE=0
 NEW_COVERAGE=0
@@ -29,47 +38,49 @@ GREEN_THRESHOLD=85
 YELLOW_THRESHOLD=50
 
 # clone and prepare gh-pages branch
-git clone -b gh-pages https://$GHE_USER:$GHE_TOKEN@github.com/$GITHUB_REPOSITORY.git .
+git clone -b gh-pages https://"$GHE_USER":"$GHE_TOKEN"@github.com/"$GITHUB_REPOSITORY".git .
 git config user.name "github-actions"
 git config user.email "github-actions@github.com"
 
-if [ ! -d "$GITHUB_WORKSPACE/gh-pages/coverage" ]; then
-    mkdir "$GITHUB_WORKSPACE/gh-pages/coverage"
-fi
+mkdir -p "$GITHUB_WORKSPACE/gh-pages/coverage/$GITHUB_REF_NAME"
+mkdir -p "$GITHUB_WORKSPACE/gh-pages/coverage/$GITHUB_SHA"
 
-if [ ! -d "$GITHUB_WORKSPACE/gh-pages/coverage/$GITHUB_REF_NAME" ]; then
-    mkdir "$GITHUB_WORKSPACE/gh-pages/coverage/$GITHUB_REF_NAME"
-fi
-
-if [ ! -d "$GITHUB_WORKSPACE/gh-pages/coverage/$GITHUB_SHA" ]; then
-	mkdir "$GITHUB_WORKSPACE/gh-pages/coverage/$GITHUB_SHA"
-fi
-
-if [ -f "$(go env GOPATH)/src/github.com/IBM/ibm-vpc-file-csi-driver/Passing" ]; then
-	curl https://img.shields.io/badge/e2e-passing-Yellow.svg > $GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/e2e.svg
-elif [ -f "$GOPATH/src/github.com/IBM/ibm-vpc-file-csi-driver/Failed" ]; then
-	curl https://img.shields.io/badge/e2e-failed-Yellow.svg > $GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/e2e.svg
-fi
-
-# Compute overall coverage percentage
+# calculate overall coverage percentage
 echo "Computing the coverages"
-OLD_COVERAGE=$(cat $GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/cover.html  | grep "%)"  | sed 's/[][()><%]/ /g' | awk '{ print $4 }' | awk '{s+=$1}END{print s/NR}')
-cp $GITHUB_WORKSPACE/cover.html $GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}
-cp $GITHUB_WORKSPACE/cover.html $GITHUB_WORKSPACE/gh-pages/coverage/$TRAVIS_COMMIT
-NEW_COVERAGE=$(cat $GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/cover.html  | grep "%)"  | sed 's/[][()><%]/ /g' | awk '{ print $4 }' | awk '{s+=$1}END{print s/NR}')
 
+if [ -f "$GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/cover.html" ]; then
+    OLD_COVERAGE=$(grep "%)" "$GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/cover.html" \
+      | sed 's/[][()><%]/ /g' | awk '{ print $4 }' \
+      | awk '{s+=$1}END{if(NR>0)print s/NR; else print 0}')
+fi
+
+cp "$GITHUB_WORKSPACE/cover.html" "$GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}"
+cp "$GITHUB_WORKSPACE/cover.html" "$GITHUB_WORKSPACE/gh-pages/coverage/$GITHUB_SHA"
+
+if [ -f "$GITHUB_WORKSPACE/Passing" ]; then
+    curl -s https://img.shields.io/badge/e2e-passing-yellow.svg \
+      > "$GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/e2e.svg"
+elif [ -f "$GITHUB_WORKSPACE/Failed" ]; then
+    curl -s https://img.shields.io/badge/e2e-failed-red.svg \
+      > "$GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/e2e.svg"
+fi
+
+NEW_COVERAGE=$(grep "%)" "$GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/cover.html" \
+  | sed 's/[][()><%]/ /g' | awk '{ print $4 }' \
+  | awk '{s+=$1}END{if(NR>0)print s/NR; else print 0}')
+
+# pick badge color
 if (( $(echo "$NEW_COVERAGE > $GREEN_THRESHOLD" | bc -l) )); then
 	BADGE_COLOR="green"
 elif (( $(echo "$NEW_COVERAGE > $YELLOW_THRESHOLD" | bc -l) )); then
 	BADGE_COLOR="yellow"
 fi
 
-# Generate badge for coverage
-curl https://img.shields.io/badge/coverage-$NEW_COVERAGE-$BADGE_COLOR.svg > $GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/badge.svg
+# generate badge
+curl -s https://img.shields.io/badge/coverage-"$NEW_COVERAGE"-$BADGE_COLOR.svg \
+  > "$GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/badge.svg"
 
-COMMIT_RANGE=(${TRAVIS_COMMIT_RANGE//.../ })
-
-# Generate result message for log and PR
+# generate result message
 if (( $(echo "$OLD_COVERAGE > $NEW_COVERAGE" | bc -l) )); then
 	RESULT_MESSAGE=":red_circle: Coverage decreased from [$OLD_COVERAGE%] to [$NEW_COVERAGE%]"
 elif (( $(echo "$OLD_COVERAGE == $NEW_COVERAGE" | bc -l) )); then
@@ -78,15 +89,19 @@ else
 	RESULT_MESSAGE=":thumbsup: Coverage increased from [$OLD_COVERAGE%] to [$NEW_COVERAGE%]"
 fi
 
-# Update gh-pages branch or PR
+# update gh-pages or PR
 echo "Updating gh-pages"
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$GITHUB_EVENT_NAME" == "push" ]; then
 	git status
 	git add .
-	git commit -m "Coverage result for commit $TRAVIS_COMMIT from build $TRAVIS_BUILD_NUMBER"
-	git push origin
-else
-        # Updates PR with coverage
-   		curl -X POST -H "Authorization: Token $GHE_TOKEN" "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$TRAVIS_PULL_REQUEST/comments" -H 'Content-Type: application/json' --data '{"body": "'"$RESULT_MESSAGE"'"}'
-
+	git commit -m "Coverage result for commit $GITHUB_SHA from run $GITHUB_RUN_NUMBER" || echo "No changes to commit"
+	git push origin gh-pages
+elif [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
+	# Extract PR number from event JSON
+	PR_NUMBER=$(jq -r .pull_request.number "$GITHUB_EVENT_PATH")
+	curl -s -X POST \
+	  -H "Authorization: token $GHE_TOKEN" \
+	  -H "Content-Type: application/json" \
+	  -d "{\"body\": \"$RESULT_MESSAGE\"}" \
+	  "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$PR_NUMBER/comments"
 fi
