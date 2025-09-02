ebc2363beac0a36c4f7cc77f1ef6da9fc2f4d148
Mahantesh-R <mahantesh.r@ibm.com>
2025-09-02 15:33:48 +0530

    adds gtihub actions related items
    Signed-off-by: Mahantesh-R <mahantesh.r@ibm.com>

--- scripts/publishCoverage.sh
+++ scripts/publishCoverage.sh
@@ -16,13 +16,9 @@
 # *****************************************************************************/
 set -x
 echo "Publishing the coverage results"
-if [ "$TRAVIS_GO_VERSION" == "tip" ]; then
-	echo "Coverage information is not required for tip version."
-	exit 0
-fi
 
-mkdir -p $TRAVIS_BUILD_DIR/gh-pages
-cd $TRAVIS_BUILD_DIR/gh-pages
+mkdir -p "$GITHUB_WORKSPACE/gh-pages"
+cd "$GITHUB_WORKSPACE/gh-pages" || return
 
 OLD_COVERAGE=0
 NEW_COVERAGE=0
@@ -33,34 +29,34 @@ GREEN_THRESHOLD=85
 YELLOW_THRESHOLD=50
 
 # clone and prepare gh-pages branch
-git clone -b gh-pages https://$GHE_USER:$GHE_TOKEN@github.com/$TRAVIS_REPO_SLUG.git .
-git config user.name "travis"
-git config user.email "travis"
+git clone -b gh-pages https://$GHE_USER:$GHE_TOKEN@github.com/$GITHUB_REPOSITORY.git .
+git config user.name "github-actions"
+git config user.email "github-actions@github.com"
 
-if [ ! -d "$TRAVIS_BUILD_DIR/gh-pages/coverage" ]; then
-	mkdir "$TRAVIS_BUILD_DIR/gh-pages/coverage"
+if [ ! -d "$GITHUB_WORKSPACE/gh-pages/coverage" ]; then
+    mkdir "$GITHUB_WORKSPACE/gh-pages/coverage"
 fi
 
-if [ ! -d "$TRAVIS_BUILD_DIR/gh-pages/coverage/$TRAVIS_BRANCH" ]; then
-	mkdir "$TRAVIS_BUILD_DIR/gh-pages/coverage/$TRAVIS_BRANCH"
+if [ ! -d "$GITHUB_WORKSPACE/gh-pages/coverage/$GITHUB_REF_NAME" ]; then
+    mkdir "$GITHUB_WORKSPACE/gh-pages/coverage/$GITHUB_REF_NAME"
 fi
 
-if [ ! -d "$TRAVIS_BUILD_DIR/gh-pages/coverage/$TRAVIS_COMMIT" ]; then
-	mkdir "$TRAVIS_BUILD_DIR/gh-pages/coverage/$TRAVIS_COMMIT"
+if [ ! -d "$GITHUB_WORKSPACE/gh-pages/coverage/$GITHUB_SHA" ]; then
+	mkdir "$GITHUB_WORKSPACE/gh-pages/coverage/$GITHUB_SHA"
 fi
 
-if [ -f "$GOPATH/src/github.com/IBM/ibm-vpc-file-csi-driver/Passing" ]; then
-	curl https://img.shields.io/badge/e2e-passing-Yellow.svg > $TRAVIS_BUILD_DIR/gh-pages/coverage/$TRAVIS_BRANCH/e2e.svg
+if [ -f "$(go env GOPATH)/src/github.com/IBM/ibm-vpc-file-csi-driver/Passing" ]; then
+	curl https://img.shields.io/badge/e2e-passing-Yellow.svg > $GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/e2e.svg
 elif [ -f "$GOPATH/src/github.com/IBM/ibm-vpc-file-csi-driver/Failed" ]; then
-	curl https://img.shields.io/badge/e2e-failed-Yellow.svg > $TRAVIS_BUILD_DIR/gh-pages/coverage/$TRAVIS_BRANCH/e2e.svg
+	curl https://img.shields.io/badge/e2e-failed-Yellow.svg > $GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/e2e.svg
 fi
 
 # Compute overall coverage percentage
 echo "Computing the coverages"
-OLD_COVERAGE=$(cat $TRAVIS_BUILD_DIR/gh-pages/coverage/$TRAVIS_BRANCH/cover.html  | grep "%)"  | sed 's/[][()><%]/ /g' | awk '{ print $4 }' | awk '{s+=$1}END{print s/NR}')
-cp $TRAVIS_BUILD_DIR/cover.html $TRAVIS_BUILD_DIR/gh-pages/coverage/$TRAVIS_BRANCH
-cp $TRAVIS_BUILD_DIR/cover.html $TRAVIS_BUILD_DIR/gh-pages/coverage/$TRAVIS_COMMIT
-NEW_COVERAGE=$(cat $TRAVIS_BUILD_DIR/gh-pages/coverage/$TRAVIS_BRANCH/cover.html  | grep "%)"  | sed 's/[][()><%]/ /g' | awk '{ print $4 }' | awk '{s+=$1}END{print s/NR}')
+OLD_COVERAGE=$(cat $GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/cover.html  | grep "%)"  | sed 's/[][()><%]/ /g' | awk '{ print $4 }' | awk '{s+=$1}END{print s/NR}')
+cp $GITHUB_WORKSPACE/cover.html $GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}
+cp $GITHUB_WORKSPACE/cover.html $GITHUB_WORKSPACE/gh-pages/coverage/$TRAVIS_COMMIT
+NEW_COVERAGE=$(cat $GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/cover.html  | grep "%)"  | sed 's/[][()><%]/ /g' | awk '{ print $4 }' | awk '{s+=$1}END{print s/NR}')
 
 if (( $(echo "$NEW_COVERAGE > $GREEN_THRESHOLD" | bc -l) )); then
 	BADGE_COLOR="green"
@@ -69,7 +65,7 @@ elif (( $(echo "$NEW_COVERAGE > $YELLOW_THRESHOLD" | bc -l) )); then
 fi
 
 # Generate badge for coverage
-curl https://img.shields.io/badge/coverage-$NEW_COVERAGE-$BADGE_COLOR.svg > $TRAVIS_BUILD_DIR/gh-pages/coverage/$TRAVIS_BRANCH/badge.svg
+curl https://img.shields.io/badge/coverage-$NEW_COVERAGE-$BADGE_COLOR.svg > $GITHUB_WORKSPACE/gh-pages/coverage/${GITHUB_REF_NAME}/badge.svg
 
 COMMIT_RANGE=(${TRAVIS_COMMIT_RANGE//.../ })
 
@@ -91,6 +87,6 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 	git push origin
 else
         # Updates PR with coverage
-   		curl -X POST -H "Authorization: Token $GHE_TOKEN" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/issues/$TRAVIS_PULL_REQUEST/comments" -H 'Content-Type: application/json' --data '{"body": "'"$RESULT_MESSAGE"'"}'
+   		curl -X POST -H "Authorization: Token $GHE_TOKEN" "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$TRAVIS_PULL_REQUEST/comments" -H 'Content-Type: application/json' --data '{"body": "'"$RESULT_MESSAGE"'"}'
 
 fi
