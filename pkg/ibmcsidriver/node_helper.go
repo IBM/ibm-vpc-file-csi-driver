83e2b0aa51b5bd90c5c59970d3a2c48bb383b7e5
Meetanshi-Gupta <meetanshi.gupta@ibm.com>
2026-07-03 18:37:34 +0530

    log messages removed
    Signed-off-by: Meetanshi-Gupta <meetanshi.gupta@ibm.com>

--- pkg/ibmcsidriver/node_helper.go
+++ pkg/ibmcsidriver/node_helper.go
@@ -80,7 +80,6 @@ func (csiNS *CSINodeServer) processMount(ctxLogger *zap.Logger, requestID, mount
 				ctxLogger.Error("Mount backend output: ", zap.String("Reponse:", errResponse))
 			}
 		}
-		ctxLogger.Info("processMount: returning CSI error-test",zap.String("requestID", requestID),zap.String("errorCode", errorCode),zap.String("fsType", fsType),zap.String("mountPath", mountPath),zap.String("targetPath", targetPath),zap.Error(err),)
 		return nil, commonError.GetCSIError(ctxLogger, errorCode, requestID, err)
 	}
 
